61b0dd6a730e2b8620c41759b911dc0c68b05bf1
Matthew  Morrison <matthew.morrison17@imperial.ac.uk>
2018-02-14 20:31:54 +0000

    Answered Question 8

--- Submission/db_2018_cw1.sql
+++ Submission/db_2018_cw1.sql
@@ -1,49 +1,46 @@
---
+
+-- psql -h db.doc.ic.ac.uk -d family_history -U lab -W -f db_2018_cw1.sql
+
 -- -- Q1 returns (name,father,mother)
---
+
 -- SELECT    name,father,mother
--- FROM      person AS child
--- WHERE     child.dod IS NOT NULL
--- AND       child.dod IN (SELECT      child.dod
---                         FROM        person AS father JOIN person AS child
---                         ON          father.name = child.father
---                         WHERE       child.dod < father.dod)
--- AND       child.dod IN (SELECT      child.dod
---                         FROM        person AS mother JOIN person AS child
---                         ON          mother.name = child.mother
---                         WHERE       child.dod < mother.dod)
+-- FROM      person
+-- WHERE     dod IS NOT NULL
+-- AND       dod IN (SELECT  child.dod
+--                   FROM    person AS father JOIN person AS child
+--                   ON      father.name = child.father
+--                   WHERE   child.dod < father.dod)
+-- AND       dod IN (SELECT  child.dod
+--                   FROM    person AS mother JOIN person AS child
+--                   ON      mother.name = child.mother
+--                   WHERE   child.dod < mother.dod)
 -- ORDER BY  name;
---
+
 -- -- Q2 returns (name)
---
--- SELECT    person.name
--- FROM      person
---           JOIN monarch
---           ON  person.name = monarch.name
--- WHERE     monarch.house IS NOT NULL
+
+-- SELECT    name
+-- FROM      monarch
+-- WHERE     house IS NOT NULL
 -- UNION
--- SELECT    person.name
--- FROM      person
---           JOIN prime_minister
---           ON  person.name = prime_minister.name
+-- SELECT    name
+-- FROM      prime_minister
 -- ORDER BY  name;
---
+
 -- -- Q3 returns (name)
---
+
 -- SELECT DISTINCT old.name
 -- FROM            ( SELECT   person.name,monarch.house,person.dod,monarch.accession
 --                   FROM     person JOIN monarch
 --                   ON       person.name = monarch.name
 --                   WHERE    monarch.house IS NOT NULL
---                 ) AS old
---           JOIN monarch AS new
+--                 ) AS old JOIN monarch AS new
 -- ON        new.house = old.house
 -- WHERE     new.accession > old.accession
 -- AND       new.accession < old.dod
 -- ORDER BY  name;
---
+
 -- -- Q4 returns (house,name,accession)
---
+
 -- SELECT    original.house,original.name,original.accession
 -- FROM      monarch AS original
 -- WHERE     original.accession <= ALL ( SELECT new.accession
@@ -51,7 +48,7 @@
 --                                       WHERE  new.house = original.house)
 -- AND       original.house IS NOT NULL
 -- ORDER BY  accession;
---
+
 -- -- Q5 returns (first_name,popularity)
 --
 -- SELECT    CASE
@@ -79,19 +76,26 @@
 
 -- Q7 returns (father,child,born)
 
-SELECT       original.name AS father, new.name AS child, COUNT(new.name) OVER (PARTITION BY original.name) AS born
-FROM         person AS original LEFT JOIN person AS new
-ON           original.name = new.father
-WHERE        (original.gender = 'M') IS TRUE;
-
-
--- SELECT    father, name, dob,
---           COUNT(name) OVER (PARTITION BY father) AS born
--- FROM      person
--- WHERE     father IS NOT NULL;
+-- SELECT      original.name AS father, new.name AS child, new.dob AS dob,
+--             CASE
+--             WHEN new.name IS NULL
+--             THEN null
+--             ELSE RANK() OVER (PARTITION BY original.name ORDER BY new.dob ASC)
+--             END AS born
+-- FROM        person AS original LEFT JOIN person AS new
+-- ON          original.name = new.father
+-- WHERE       (original.gender = 'M') IS TRUE
+-- ORDER BY    father, born;
 
 -- Q8 returns (monarch,prime_minister)
 
-;
-
--- psql -h db.doc.ic.ac.uk -d family_history -U lab -W -f db_2018_cw1.sql
+-- SELECT DISTINCT monarch_list.name AS monarch, prime_minister.name AS prime_minister
+-- FROM            (SELECT        original.name, MIN(original.accession) AS start_date, MIN(new.accession) AS end_date
+--                 FROM          monarch AS original JOIN monarch AS new
+--                 ON            original.house = new.house
+--                 WHERE         original.accession >= new.accession IS FALSE
+--                 GROUP BY      original.name
+--                 ORDER BY      MIN(original.accession)) AS monarch_list CROSS JOIN prime_minister
+-- WHERE           prime_minister.entry > monarch_list.start_date IS TRUE
+-- AND             prime_minister.entry < monarch_list.end_date IS TRUE
+-- ORDER BY        monarch, prime_minister;
